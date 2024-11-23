303775aa2368e43f238d36388bf38dd3544f2a9b
dideng地灯 <145828088+837463637@users.noreply.github.com>
2024-11-23 12:06:38 +0800

    Add files via upload

---
+++ Wo's kitchen Launcher/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.Logo_home_1 = new System.Windows.Forms.Label();
+            this.DownloadHome = new System.Windows.Forms.Button();
+            this.StartupHome = new System.Windows.Forms.Button();
+            this.version_bata_1_0_0 = new System.Windows.Forms.Button();
+            this.version_label_home = new System.Windows.Forms.Label();
+            this.version_bata_1_0_1 = new System.Windows.Forms.Button();
+            this.version_bata_1_0_2 = new System.Windows.Forms.Button();
+            this.version_bata_label = new System.Windows.Forms.Label();
+            this.NoDownload = new System.Windows.Forms.Button();
+            this.Download_home_Python = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Logo_home_1
+            // 
+            resources.ApplyResources(this.Logo_home_1, "Logo_home_1");
+            this.Logo_home_1.BackColor = System.Drawing.Color.Transparent;
+            this.Logo_home_1.Name = "Logo_home_1";
+            // 
+            // DownloadHome
+            // 
+            resources.ApplyResources(this.DownloadHome, "DownloadHome");
+            this.DownloadHome.Name = "DownloadHome";
+            this.DownloadHome.UseVisualStyleBackColor = true;
+            this.DownloadHome.Click += new System.EventHandler(this.DownloadHomeOperate);
+            // 
+            // StartupHome
+            // 
+            resources.ApplyResources(this.StartupHome, "StartupHome");
+            this.StartupHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.StartupHome.Name = "StartupHome";
+            this.StartupHome.UseVisualStyleBackColor = true;
+            this.StartupHome.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // version_bata_1_0_0
+            // 
+            this.version_bata_1_0_0.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.version_bata_1_0_0, "version_bata_1_0_0");
+            this.version_bata_1_0_0.Name = "version_bata_1_0_0";
+            this.version_bata_1_0_0.UseVisualStyleBackColor = false;
+            this.version_bata_1_0_0.Click += new System.EventHandler(this.Download_1_0_0);
+            // 
+            // version_label_home
+            // 
+            resources.ApplyResources(this.version_label_home, "version_label_home");
+            this.version_label_home.BackColor = System.Drawing.Color.Transparent;
+            this.version_label_home.Name = "version_label_home";
+            this.version_label_home.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // version_bata_1_0_1
+            // 
+            resources.ApplyResources(this.version_bata_1_0_1, "version_bata_1_0_1");
+            this.version_bata_1_0_1.Name = "version_bata_1_0_1";
+            this.version_bata_1_0_1.UseVisualStyleBackColor = true;
+            this.version_bata_1_0_1.Click += new System.EventHandler(this.Download_1_0_1);
+            // 
+            // version_bata_1_0_2
+            // 
+            this.version_bata_1_0_2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.version_bata_1_0_2, "version_bata_1_0_2");
+            this.version_bata_1_0_2.Name = "version_bata_1_0_2";
+            this.version_bata_1_0_2.UseVisualStyleBackColor = false;
+            this.version_bata_1_0_2.Click += new System.EventHandler(this.Download_1_0_2);
+            // 
+            // version_bata_label
+            // 
+            resources.ApplyResources(this.version_bata_label, "version_bata_label");
+            this.version_bata_label.BackColor = System.Drawing.Color.Transparent;
+            this.version_bata_label.Name = "version_bata_label";
+            // 
+            // NoDownload
+            // 
+            resources.ApplyResources(this.NoDownload, "NoDownload");
+            this.NoDownload.Name = "NoDownload";
+            this.NoDownload.UseVisualStyleBackColor = true;
+            this.NoDownload.Click += new System.EventHandler(this.NoDownloaHome);
+            // 
+            // Download_home_Python
+            // 
+            resources.ApplyResources(this.Download_home_Python, "Download_home_Python");
+            this.Download_home_Python.Name = "Download_home_Python";
+            this.Download_home_Python.UseVisualStyleBackColor = true;
+            this.Download_home_Python.Click += new System.EventHandler(this.DownloadPython);
+            // 
+            // MainWindow
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.Download_home_Python);
+            this.Controls.Add(this.NoDownload);
+            this.Controls.Add(this.version_bata_label);
+            this.Controls.Add(this.version_bata_1_0_2);
+            this.Controls.Add(this.version_bata_1_0_1);
+            this.Controls.Add(this.version_label_home);
+            this.Controls.Add(this.version_bata_1_0_0);
+            this.Controls.Add(this.StartupHome);
+            this.Controls.Add(this.DownloadHome);
+            this.Controls.Add(this.Logo_home_1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainWindow";
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Logo_home_1;
+        private System.Windows.Forms.Button DownloadHome;
+        private System.Windows.Forms.Button StartupHome;
+        private System.Windows.Forms.Button version_bata_1_0_0;
+        private System.Windows.Forms.Label version_label_home;
+        private System.Windows.Forms.Button version_bata_1_0_1;
+        private System.Windows.Forms.Button version_bata_1_0_2;
+        private System.Windows.Forms.Label version_bata_label;
+        private System.Windows.Forms.Button NoDownload;
+        private System.Windows.Forms.Button Download_home_Python;
+    }
+}
+
---
+++ Wo's kitchen Launcher/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class MainWindow : Form{
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("2", "提示"); // 点击按钮时显示消息框
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void DownloadHomeOperate(object sender, EventArgs e) {
+            DownloadHome.Visible = false;
+            StartupHome.Visible = false;
+            Logo_home_1.Visible = false;
+            version_label_home.Visible = true;
+            version_bata_1_0_0.Visible = true;
+            version_bata_1_0_1.Visible = true;
+            version_bata_1_0_2.Visible = true;
+            version_bata_label.Visible = true;
+            NoDownload.Visible = true;
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void NoDownloaHome(object sender, EventArgs e)
+        {
+            DownloadHome.Visible = true;
+            StartupHome.Visible = true;
+            Logo_home_1.Visible = true;
+            version_label_home.Visible = false;
+            version_bata_1_0_0.Visible = false;
+            version_bata_1_0_1.Visible = false;
+            version_bata_1_0_2.Visible = false;
+            version_bata_label.Visible = false;
+            NoDownload.Visible = false;
+        }
+        private async void Download_1_0_0(object sender, EventArgs e)
+        {
+            MessageBox.Show("请稍等，正在下载！（下载时程序会卡顿）");
+            const string url = "https://github.com/837463637/Wo-s-kitchen/releases/download/Game1.0.0bata.zip";
+            var client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+            var content = response.Content.ReadAsByteArrayAsync().Result;
+            var filePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Game1.0.0bata.zip");
+            System.IO.File.WriteAllBytes(filePath, content);
+            await Task.Delay(1000);
+            File.Move("Game1.0.0bata.zip", "C:\\Users\\Administrator\\Desktop\\");
+            ZipFile.ExtractToDirectory("Game1.0.0bata.zip", "C:\\Users\\Administrator\\Desktop\\");
+            MessageBox.Show("下载完成，请按： 下载Python环境 进行安装！");
+        
+        }
+        private async void Download_1_0_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("请稍等，正在下载！（下载时程序会卡顿）");
+            const string url = "https://github.com/837463637/Wo-s-kitchen/releases/download/Game1.0.1/Game1.0.1bata.zip";
+            var client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+            var content = response.Content.ReadAsByteArrayAsync().Result;
+            var filePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Game1.0.1bata.zip");
+            System.IO.File.WriteAllBytes(filePath, content);
+            await Task.Delay(1000);
+            File.Move("Game1.0.1bata.zip", "C:\\Users\\Administrator\\Desktop\\");
+            ZipFile.ExtractToDirectory("Game1.0.1bata.zip", "C:\\Users\\Administrator\\Desktop\\");
+            MessageBox.Show("下载完成，请按： 下载Python环境 进行安装！");
+
+        }
+        private async void Download_1_0_2(object sender, EventArgs e)
+        {
+            MessageBox.Show("请稍等，正在下载！（下载时程序会卡顿）");
+            const string url = "https://github.com/837463637/Wo-s-kitchen/releases/download/Game1.0.2/Wo-s-kitchen.zip";
+            var client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+            var content = response.Content.ReadAsByteArrayAsync().Result;
+            var filePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "Wo-s-kitchen.zip");
+            await Task.Delay(1000);
+            await Task.Delay(1000);
+            File.Move("Game1.0.2bata.zip", "C:\\Users\\Administrator\\Desktop\\");
+            ZipFile.ExtractToDirectory("Wo-s-kitchen.zip", "C:\\Users\\Administrator\\Desktop\\");
+            MessageBox.Show("下载完成，请按： 下载Python环境 进行安装！");
+        }
+        private void DownloadPython(object sender, EventArgs e)
+        {
+            MessageBox.Show("正在下载Python环境，请稍等！（下载时程序会卡顿）");
+            const string url = "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe";
+            var client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+            var content = response.Content.ReadAsByteArrayAsync().Result;
+            var filePath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "python-3.9.6-amd64.exe");
+            System.IO.File.WriteAllBytes(filePath, content);
+            System.Diagnostics.Process.Start(filePath);
+            MessageBox.Show("下载完成,正在下载外部库，请稍等！（下载时程序会卡顿）");
+            string batFilePath = "Python.bat";
+            Process.Start(batFilePath);
+            MessageBox.Show("下载完成，请按： 启动游戏 进行游戏！");
+        }
+        private void StartGame(object sender, EventArgs e)
+        {
+            Process.Start("C:\\Users\\Administrator\\Desktop\\python.exe", "C:\\Users\\Administrator\\Desktop\\main.py");
+        }
+    }
+}
